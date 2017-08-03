9d13a255b3c6018d5d11b05fb447fd9f63168d98
Ceralor <ceralor@gmail.com>
2017-08-03 05:10:57 -0500

    Fixed NSFW indicator text

--- picartopush.py
+++ picartopush.py
@@ -7,7 +7,6 @@ try:
 except:
     print("File 'config.json' must be present!")
     exit()
-
 try:
     with open("online_following.json","r") as f:
         previous_online_following = json.load(f)
@@ -15,7 +14,6 @@ except:
     with open("online_following.json","w") as f:
         json.dump({},f)
     print("File 'online_following.json' has been created for you.")
-
 headers = {'accept':'application/json','authorization':'Bearer %s' % (config['picarto_bearer'])}
 r = requests.get('https://api.picarto.tv/v1/user',headers=headers)
 following = r.json()['following']
@@ -28,8 +26,8 @@ online_following
 with open("online_following.json","w") as f:
     json.dump([ x['name'] for x in online_following ],f)
 for channel in [x for x in online_following if x['name'] not in previous_online_following]:
-    nsfw_string = " (NSFW)" if channel['adult'] else None,
-    title = "Picarto user %s online!%s" % (channel['name'],nsfw_string)
-    msg = """%s is now streaming%s, check it out! https://picarto.tv/%s""" % (channel['name'],nsfw_string,channel['name'])
+    nsfw_string = ' (NSFW)' if channel['adult'] else ''
+    title = 'Picarto user %s online!%s' % (channel['name'],nsfw_string)
+    msg = '%s is now streaming%s, check it out! https://picarto.tv/%s' % (channel['name'],nsfw_string,channel['name'])
     simplepush.send(config['simplepush_key'],title,msg)
     print("%s is online!" % (channel['name']))
